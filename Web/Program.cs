15c9903dba73bd659145fd0a2f71c6543406115c
arczi <artur.bula2001@gmail.com>
2024-04-19 09:44:50 +0200

    intro swagger, automapper, razor, dependencies

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,7 @@
+using Business.Profiles;
 using DataAccess.Ef.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +10,33 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContextPool<MyDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("MsSQLLocalDB")));
 
+//DI
+Business.Dependencies.Register(builder.Services);
+
+//Razor pages
+builder.Services.AddRazorPages();
+
+//Automapper
+builder.Services.AddAutoMapper(typeof(FlightProfile));
+
+//Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseStaticFiles();
 
