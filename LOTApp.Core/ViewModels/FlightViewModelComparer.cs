d1186086aa8119f7775545ffd02e582495ad944b
arczi <artur.bula2001@gmail.com>
2024-04-21 23:56:47 +0200

    intro comparer, validator

---
+++ LOTApp.Core/ViewModels/FlightViewModelComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace LOTApp.Core.ViewModels
+{
+    public class FlightViewModelComparer : IEqualityComparer<FlightViewModel>
+    {
+        public bool Equals(FlightViewModel? x, FlightViewModel? y)
+        {
+            if (x == null || y == null) return false;
+            return
+              x.Id == y.Id &&
+              x.FlightNumber == y.FlightNumber &&
+              x.DepartTime == y.DepartTime &&
+              x.DepartLocation == y.DepartLocation &&
+              x.ArrivalLocation == y.ArrivalLocation &&
+              x.PlaneType == y.PlaneType;
+        }
+
+        public int GetHashCode(FlightViewModel obj)
+        {
+            // Combine hash codes of relevant properties for consistent comparison
+            return obj.Id.GetHashCode() ^
+                   obj.FlightNumber.GetHashCode() ^
+                   obj.DepartTime.GetHashCode() ^
+                   obj.DepartLocation.GetHashCode() ^
+                   obj.ArrivalLocation.GetHashCode() ^
+                   obj.PlaneType.GetHashCode();
+        }
+    }
+}
